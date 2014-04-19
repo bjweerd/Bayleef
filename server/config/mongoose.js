d3fd8ea8bebae05e58f36bbbb25324f6a1aee869
Kamin Airnomad <kamintears@gmail.com>
2014-04-20 00:48:07 +0200

    serverside

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,20 @@ module.exports = function(config) {
     db.once('open', function callback() {
 	console.log("The 'bayleef' db at [ " + config.db + " ] opened");
     });
+
+    var userSchema = mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	userName: String
+    });
+
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function(err, collection) {
+	if (collection.length === 0) {
+	    User.create({firstName:'Site', lastName:"Administrator", userName:"admin"});
+	    User.create({firstName:'John', lastName:"DoeDea", userName:"john"});
+	    User.create({firstName:'Mary', lastName:"DoeDoe", userName:"mary"});
+	}
+    });
 }
